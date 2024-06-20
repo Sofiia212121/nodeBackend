c2bfc7e0f23b8c8ad695e3a4cfba4eb850ea83c2
Sofiia212121 <sofiiamizina21@gmail.com>
2024-06-20 20:27:00 +0300

    Env vars used

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -6,11 +7,11 @@ const port = 3000
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'node_backend_db',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
 });
 
