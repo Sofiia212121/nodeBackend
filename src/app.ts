5921c36e212cd010297906972f59109a912217f0
Sofiia212121 <sofiiamizina21@gmail.com>
2024-10-08 21:16:43 +0300

    AuthMiddleWare created

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 import { AppDataSource } from './data-source';
 import { getAllUsers, getUserById, createUser, updateUser, deleteUser, updateUserPassword, loginUser } from './controllers/UserController';
 
@@ -11,16 +13,43 @@ const PORT = 3000;
 
 app.use(express.json());
 
+const authorizationMiddleWare = function (req: Request, res: Response, next: NextFunction) {
+    const authoHeader: string = req.headers?.authorization || '';
+
+    let token, user;
+
+    if (authoHeader.startsWith('Bearer ')) {
+        token = authoHeader.slice(7);
+    } else {
+        return res.status(401).json({ message: 'Invalid authorization header' });
+    }
+
+    try {
+        user = jwt.verify(token, process.env.JWT_SECRET || '');
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    console.log(user);
+
+    next();
+};
+
 AppDataSource.initialize().then(() => {
     console.log('Connected to the database');
 
-    app.patch('/users/update-password/:id', updateUserPassword);
-    app.get('/users', getAllUsers);
-    app.get('/users/:id', getUserById);
-    app.post('/users', createUser);
-    app.patch('/users/:id', updateUser);
-    app.delete('/users/:id', deleteUser);
+    const usersRouter = express.Router();
+    usersRouter.use(authorizationMiddleWare);
+
+    usersRouter.patch('/update-password/:id', updateUserPassword);
+    usersRouter.get('/', getAllUsers);
+    usersRouter.get('/:id', getUserById);
+    usersRouter.patch('/:id', updateUser);
+    usersRouter.delete('/:id', deleteUser);
+    app.use('/users', usersRouter);
+    
     app.post('/login', loginUser);
+    app.post('/register', createUser);
 
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
